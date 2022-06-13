b7f86e42401b4b6b7457f7519ed4f26049b95ff8
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-06-13 16:26:57 +0530

    init

--- InterviewBit/Bit/3-reverseBit.cpp
+++ InterviewBit/Bit/3-reverseBit.cpp
@@ -12,3 +12,18 @@ unsigned int Solution::reverse(unsigned int A) {
     }
     return ans;
 }
+
+
+//Using OR operator
+
+
+unsigned int Solution::reverse(unsigned int A) {
+    unsigned int b=0;
+    for(int i=0;i<32;i++)
+    {
+        b=b<<1;
+        b=b|A&1;
+        A=A>>1;
+    }
+    return b; 
+}
